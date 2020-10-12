0d72ac176e6ba290674509405e6a1af4c75d4090
chiara <chiara7.brambilla@mail.polimi.it>
2020-10-12 09:39:17 +0200

    Texture

--- sketch.js
+++ sketch.js
@@ -3,11 +3,9 @@ function preload() {}
 let w, h, b1, b2, b3, tt, tt2, tt3;
 let letterColor;
 let color1, color2;
-var myColors =['#030f40', '#f478f5', '#5ba3aa', '#538be0', '#8f7ceb'];
+var myColors = ['Orange', 'Red', 'Tomato', '#538be0', '#8f7ceb'];
 w = 50;
 h = 50;
-
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -35,8 +33,8 @@ function setup() {
 function draw() {
 
   //BOTTONE 1
-  b1 = createButton('TEXTURE 1');
-  b1.position((width - 370), (height - 100));
+  b1 = createButton('TEXTURE 1 + random');
+  b1.position((width - 370), (height - 130));
   b1.size(150);
   b1.style('background-color', color1);
   b1.style('font-size', '15px');
@@ -51,8 +49,8 @@ function draw() {
   b1.mousePressed(modOne);
 
   //BOTTONE 2
-  b2 = createButton('TEXTURE 2');
-  b2.position((width - 200), (height - 100));
+  b2 = createButton('TEXTURE 2 + random');
+  b2.position((width - 200), (height - 130));
   b2.size(150);
   b2.style('background-color', color1);
   b2.style('font-size', '15px');
@@ -105,9 +103,9 @@ function modOne() {
       fill(color2);
       rect(x, y, w, h);
       fill(color1);
-      rect(x, y, w / 1.5, h / 1.5);
+      rect(x, y, w / 1.5*random(0.5, 1), h / 1.5*random(0.5, 1));
       fill(color2);
-      rect(x, y, w / 2.5, h / 2.5);
+      rect(x, y, w / 2.5*noise(random(1, 2)), h / 2.5*noise(random(1, 2)));
     }
   }
   comandi();
@@ -116,27 +114,43 @@ function modOne() {
 
 function modTwe() {
 
-background(color1);
-comandi();
-stroke(color1);
-for(var x = 0; x < width; x += 100) {
-    for(var y = 0; y < height; y += 100) {
+  background(color1);
+  comandi();
+  stroke(color1);
+  for (var x = 0; x < width; x += 100) {
+    for (var y = 0; y < height; y += 100) {
 
       var index = floor(random());
-      fill(color1);
-      triangle(x, y, x, y+100, x+50, y+50);
-
-      var index = floor(random());
-      fill(color2);
-      triangle(x, y, x+100, y, x+50, y+50);
-
-      var index = floor(random());
+      if (random() < 0.5) {
         fill(color1);
-        triangle(x+100, y, x+100, y+100, x+50, y+50);
+      } else {
+        fill(color2);
+      }
+      triangle(x, y, x, y + 100, x + 50, y + 50);
 
       var index = floor(random());
-    fill(color2);
-      triangle(x, y+100, x+100, y+100, x+50, y+50);
+      if (random() < 0.5) {
+        fill(color2);
+      } else {
+        fill(color1);
+      }
+      triangle(x, y, x + 100, y, x + 50, y + 50);
+
+      var index = floor(random());
+      if (random() < 0.5) {
+        fill(color1);
+      } else {
+        fill(color2);
+      }
+      triangle(x + 100, y, x + 100, y + 100, x + 50, y + 50);
+
+      var index = floor(random());
+      if (random() < 0.5) {
+        fill(color2);
+      } else {
+        fill(color1);
+      }
+      triangle(x, y + 100, x + 100, y + 100, x + 50, y + 50);
     }
   }
 
@@ -145,24 +159,25 @@ for(var x = 0; x < width; x += 100) {
 
 
 
-function keyReleased(){
-  if (key == 's' || key == 'S')
-  {save('Texture.png');
-}
+function keyReleased() {
+  if (key == 's' || key == 'S') {
+    save('Texture.png');
+  }
 
-if (key == '1') {
+  if (key == '1') {
 
- for(var x=25; x<width; x+=50){
-   for(var y=25; y<height; y+=50){
-     var r = random(0,myColors.length -1);
-     frameRate(5);
-          r = Math.round(r);
-          noFill();
-          stroke(myColors[r]);
-          strokeWeight(0.3);
-      rect(x, y, 50,50);}
- }
-}
+    for (var x = 25; x < width; x += 50) {
+      for (var y = 25; y < height; y += 50) {
+        var r = random(0, myColors.length - 1);
+        frameRate(5);
+        r = Math.round(r);
+        noFill();
+        stroke(myColors[r]);
+        strokeWeight(0.3);
+        rect(x, y, 50, 50);
+      }
+    }
+  }
 
   if (key == '2') {
     color1 = color('Teal');
@@ -171,7 +186,7 @@ if (key == '1') {
     comandi();
   }
 
-   if (key == '3') {
+  if (key == '3') {
     color1 = color('PaleVioletRed');
     color2 = color('Pink');
     background(color1);
